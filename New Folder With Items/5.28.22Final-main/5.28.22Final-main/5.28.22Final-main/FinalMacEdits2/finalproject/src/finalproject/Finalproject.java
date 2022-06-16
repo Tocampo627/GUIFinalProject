155a2bad0419586c46e0eea01653ef3a1accd012
Tanya Ocampo <91620236+Tocampo627@users.noreply.github.com>
2022-06-16 15:19:42 -0500

    Add files via upload

---
+++ New Folder With Items/5.28.22Final-main/5.28.22Final-main/5.28.22Final-main/FinalMacEdits2/finalproject/src/finalproject/Finalproject.java
@@ -0,0 +1,13 @@
+
+package finalproject;
+
+import FinalView.EmployeeGUI;// importing the folder 
+
+public class Finalproject {
+
+   
+    public static void main(String[] args) {
+       new EmployeeGUI();
+    }
+    
+}
